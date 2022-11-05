943967e331047abc9ad84818a7dba4e539f3b0d2
Ahan <64554828+AhanR@users.noreply.github.com>
2022-11-06 04:11:42 +0530

    added content and content controls

--- src/pages/canvas.jsx
+++ src/pages/canvas.jsx
@@ -1,21 +1,86 @@
-import { Component, createRef } from "react";
+import { Component, createRef, useContext } from "react";
 
 export default class Canvas extends Component {
     
     constructor(props) {
         super(props);
         this.canvasRef = createRef();
+        this.width = parseInt(this.props.width);
+        this.height = parseInt(this.props.height);
+        
+        // point is the draw size for each point in the animation
+        this.point = 5;
+        
+        // the rate helps control the speed of the reaction
+        // distortion tells us how much to distort the shapes
+        this.rate = 0.1;
+        this.distortion = Math.random(this.props.seed) * 20;
+
+        // t : theta (angle for rotation)
+        // r : radius of rotation
+        this.r = Math.floor(10*Math.random(this.props.seed));
+        this.dr = Math.random(this.props.seed) + 0.1;
+
+        this.t = 0;
+        this.dt = Math.PI/24;
+
+        
+        this.color = "#"+Math.floor( Math.random(this.props.seed) * 1e6 );
+
+        // console.log("new canvas distortion : ", this.distortion);
     }
     
     componentDidMount () {
-        this.value = Math.random(this.props.seed);
-        this.updateCanvas();
+        // console.log("initial view created");
+        this.ctx = this.canvasRef.current.getContext('2d');
     }
 
-    updateCanvas () {
-        this.ctx = this.canvasRef.current.getContext('2d');
-        this.ctx.fillRect(0,this.value*window.innerHeight, 100, 100);
-        this.ctx.fillStyle = "#"+parseInt(this.value*1000000);
+    componentDidUpdate () {
+        // console.log("updating view");
+        this.play();
+    }
+
+    draw = (x,y) => {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        this.ctx.fillStyle = this.color;
+        this.ctx.rect(x + this.width/2,y + this.height/2, this.point, this.point)
+        this.ctx.fill();
+    }
+
+    write = (text) => {
+        this.ctx.font = "20px Monospace";
+        this.ctx.fillStyle = "white";
+        this.ctx.fillText(text, this.width/2 - 5*text.length , this.height/2 + 10);
+    }
+    
+    play = () => {
+        console.log("a canvas is running");
+
+        if(this.r > this.height*0.35) {
+            this.write("animation complete");
+            return;
+        };
+
+        //check if the slide is visible
+        if(this.props.inView != this.props.slide) {
+            return;
+        }
+
+        this.t += this.rate*this.dt;
+        this.r += this.rate*this.dr;
+
+        this.draw(this.r*Math.sin(this.t) + 20*Math.cos(this.r) , this.r*Math.cos(this.t) + 20*Math.sin(this.r));
+        
+        this.animationId = requestAnimationFrame(this.play);
+    }
+
+    pause = () => {
+        // console.log("canvas paused");
+        cancelAnimationFrame(this.animationId);
+    }
+
+    componentWillUnmount() {
+        this.pause();
     }
 
     render() {
@@ -23,8 +88,8 @@ export default class Canvas extends Component {
             <canvas
                 ref={this.canvasRef}
                 className={this.props.className}
-                width={parseFloat(this.props.width)*window.innerWidth}
-                height={parseFloat(this.props.height)*window.innerHeight}
+                width={this.props.width}
+                height={this.props.height}
             />
         );
     }
--- src/pages/slides.jsx
+++ src/pages/slides.jsx
@@ -5,25 +5,43 @@ import style from './utilities.module.css'
 import { Context } from './context';
 import { Link } from 'react-router-dom';
 
+export const PageContext = React.createContext();
+
 export default function Slides() {
     
     const { count, setCount } = useContext(Context);
-    // addCanvas(count);
 
-    const width = 0.3;
-    const height = 1;
+    const width = window.innerWidth*0.3;
+    const height = window.innerHeight;
     const minimum = 1;
     const [seed, setSeed] = useState(0);
     const [buffer, setBuffer] = useState([[count,seed]]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [inView, setInView] = useState(1);
 
     //setting count to zero on mount
     useEffect(() => {
+        console.log(buffer[inView]);
         setCount(minimum);
+        return () => {
+            setInView(-1);
+        }
     }, [])
 
     const scroll = evt => {
-        const scrollIndex = evt.currentTarget.scrollTop - (count - minimum)* window.innerHeight;
+        const scrollIndex = evt.currentTarget.scrollTop - (count - minimum)* height;
+
+        const slideNumber = Math.floor((evt.currentTarget.scrollTop + height / 2) / height);
+
+        if(inView != slideNumber) {
+            // pause the previous slide
+
+            // play the new slide
+            setInView(slideNumber);
+            console.log(slideNumber);
+        }
+
         if (scrollIndex > 0) {
             setLoading(true);
             setCount(count => count + 1);
@@ -32,6 +50,7 @@ export default function Slides() {
     }
 
     function addCanvas(count) {
+        setError(false);
         fetch("https://randommer.io/api/SocialNumber",
             {
                 method: "GET",
@@ -47,24 +66,44 @@ export default function Slides() {
             setSeed(parseInt(newSeed));
             setBuffer(buf => [...buf, [newSeed, count]]);
             setLoading(false);
+        })
+        .catch(e => {
+            console.log(e.message);
+            setLoading(false);
+            setError(e.message);
         });
     }
 
     return (
         <section
-            className={` w-[30vw] h-[100vh] m-auto relative overflow-y-scroll border-x-2 border-white scroll-smooth `}
+            className={` m-auto relative overflow-y-scroll border-x-2 border-white scroll-smooth `}
             onScroll={scroll}
+            style = {{
+                width : width,
+                height : height
+            }}
         >
             <div className={` sticky top-0 left-0 w-[100%] bg-white flex flex-wrap justify-between p-4 `} >
                 <h1 className=' w-[100%] text-2xl font-bold ' >Slides</h1>
                 <Link to={"/"} className=" underline " >Home</Link>
                 <div>buffer : {buffer.length}</div>
-                <div>seed : {seed}</div>
+                <div>seed : { buffer[inView] }</div>
             </div>
-            {
-                buffer.map(identity => <Canvas seed={identity[0]} width={width} height={height} className=" border-y-2 border-white bg-neutral-900 border-spacing-0" key={identity[1]} />)
-            }
-            <div className={` text-white text-center p-4 ${style.loading} ${loading ? "" : "hidden"}`}>Loading..</div>
+            <PageContext.Provider value={{ inView }}>
+                {
+                    buffer.map(identity => <Canvas 
+                        seed={identity[0]} 
+                        width={width} 
+                        height={height} 
+                        className=" border-y-2 border-white bg-neutral-900 border-spacing-0" 
+                        key={identity[1]} 
+                        inView = {inView}
+                        slide = {identity[1]}
+                    />)
+                }
+            </PageContext.Provider>
+            <div className={` text-white bg-sky-800 text-center p-4 ${style.loading} ${loading ? "" : "hidden"}`}>Loading..</div>
+            <div className={` text-white bg-red-700 text-center p-4 ${error ? "" : "hidden"}`}>{error}</div>
         </section>
     )
 }
